02d601e8a011a706a4ae703d234466a2ad4701b1
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2021-12-14 16:42:31 +0100

    build

--- src/components/ui/CallToScroll.jsx
+++ src/components/ui/CallToScroll.jsx
@@ -2,8 +2,19 @@ import clsx from "clsx"
 import React from "react"
 
 const CallToScroll = ({ color = "black" }) => {
+  const _onClick = () => {
+    const windowHeight = window.innerHeight
+    // window.scrollBy(0, windowHeight)
+    window.scrollTo({ top: windowHeight, behavior: "smooth" })
+  }
+
   return (
-    <div className={clsx("absolute text-center bottom-0 left-0 w-screen p-md")}>
+    <div
+      className={clsx(
+        "absolute text-center bottom-0 left-0 w-screen p-md cursor-pointer"
+      )}
+      onClick={_onClick}
+    >
       <i
         className={clsx(
           "icon-chevron-s animate-bounce inline-block",
