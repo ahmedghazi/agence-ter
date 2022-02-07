61c120ce34c74d4ddd40041f89e9eb9bbb348d9e
ahmedghazi <atmet.ghazi@gmail.com>
2022-02-07 17:34:29 +0100

    build

--- src/components/ui/ScrollToTop.jsx
+++ src/components/ui/ScrollToTop.jsx
@@ -1,14 +1,39 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import clsx from "clsx"
 import { _localizeText } from "../../core/utils"
 
 const ScrollToTop = () => {
+  const [hide, setHide] = useState()
+
+  useEffect(() => {
+    _onScroll()
+    window.addEventListener("scroll", _onScroll)
+
+    return () => window.removeEventListener("scroll", _onScroll)
+  }, [])
+
+  const _onScroll = () => {
+    // console.log(window.pageYOffset)
+    if (window.pageYOffset < window.innerHeight) {
+      setHide(true)
+    } else {
+      setHide(false)
+    }
+  }
+
   const _onClick = () => {
     // window.scrollTo(0, 0)
     window.scrollTo({ top: 0, behavior: "smooth" })
   }
 
   return (
-    <div className=" text-center cursor-pointer" onClick={() => _onClick()}>
+    <div
+      className={clsx(
+        "fixed bottom-md right-md text-center cursor-pointer transition-all",
+        hide ? "slideDown" : ""
+      )}
+      onClick={() => _onClick()}
+    >
       <div
         className="absolute left-1/2 -top-full "
         style={{ transform: "translate(-50%, -20%)" }}
