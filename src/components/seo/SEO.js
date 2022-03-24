ea24b34673556a7d48fea849d25a76548d47c01a
ahmedghazi <atmet.ghazi@gmail.com>
2022-03-24 14:46:20 +0100

    build

--- src/components/seo/SEO.js
+++ src/components/seo/SEO.js
@@ -1,37 +1,42 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
-import Facebook from './Facebook'
-import Twitter from './Twitter'
-
+import React from "react"
+import Helmet from "react-helmet"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import Facebook from "./Facebook"
+import Twitter from "./Twitter"
 
 // Complete tutorial: https://www.gatsbyjs.org/docs/add-seo-component/
 
-const SEO = ({ pageTitle, pageDescription, pageBanner, pathname, page, template }) => {
+const SEO = ({
+  pageTitle,
+  pageDescription,
+  pageBanner,
+  pathname,
+  page,
+  template,
+}) => {
   const { site } = useStaticQuery(query)
   const {
     buildTime,
-    siteMetadata: { 
-      siteTitle, 
-      siteDescription, 
-      siteUrl, 
-      defaultBanner, author, 
-      twitter, 
-      facebook 
+    siteMetadata: {
+      siteTitle,
+      siteDescription,
+      siteUrl,
+      defaultBanner,
+      author,
+      twitter,
+      facebook,
     },
   } = site
-  
 
   //const localizedPath = i18n[locale].default ? '' : `/${i18n[locale].path}`
   const homeURL = `${siteUrl}`
 
-
   const seo = {
-    title: page ? pageTitle+" - "+siteTitle : siteTitle,
+    title: page ? pageTitle + " - " + siteTitle : siteTitle,
     description: page ? pageDescription : siteDescription,
     image: defaultBanner,
-    url: `${siteUrl}${pathname || ''}`,
+    url: `${siteUrl}${pathname || ""}`,
   }
 
   // schema.org in JSONLD format
@@ -39,8 +44,8 @@ const SEO = ({ pageTitle, pageDescription, pageBanner, pathname, page, template 
   // You can fill out the 'author', 'creator' with more data or another type (e.g. 'Organization')
 
   const schemaOrgWebPage = {
-    '@context': 'http://schema.org',
-    '@type': 'WebPage',
+    "@context": "http://schema.org",
+    "@type": "WebPage",
     url: homeURL,
     //headline,
     //inLanguage: siteLanguage,
@@ -48,57 +53,54 @@ const SEO = ({ pageTitle, pageDescription, pageBanner, pathname, page, template 
     description: seo.description,
     name: seo.title,
     author: {
-      '@type': 'Person',
+      "@type": "Person",
       name: author,
     },
     copyrightHolder: {
-      '@type': 'Person',
+      "@type": "Person",
       name: author,
     },
-    copyrightYear: '2019',
+    copyrightYear: "2019",
     creator: {
-      '@type': 'Person',
+      "@type": "Person",
       name: author,
     },
     publisher: {
-      '@type': 'Person',
+      "@type": "Person",
       name: author,
     },
     //datePublished: '2019-01-18T10:30:00+01:00',
     dateModified: buildTime,
     image: {
-      '@type': 'ImageObject',
+      "@type": "ImageObject",
       url: `${siteUrl}${defaultBanner}`,
     },
   }
 
-
-
   let schemaArticle = null
 
   if (page) {
-    
     schemaArticle = {
-      '@context': 'http://schema.org',
-      '@type': 'Article',
+      "@context": "http://schema.org",
+      "@type": "Article",
       author: {
-        '@type': 'Person',
+        "@type": "Person",
         name: author,
       },
       copyrightHolder: {
-        '@type': 'Person',
+        "@type": "Person",
         name: author,
       },
-      copyrightYear: '2019',
+      copyrightYear: "2019",
       creator: {
-        '@type': 'Person',
+        "@type": "Person",
         name: author,
       },
       publisher: {
-        '@type': 'Organization',
+        "@type": "Organization",
         name: author,
         logo: {
-          '@type': 'ImageObject',
+          "@type": "ImageObject",
           url: `${siteUrl}${defaultBanner}`,
         },
       },
@@ -110,12 +112,11 @@ const SEO = ({ pageTitle, pageDescription, pageBanner, pathname, page, template 
       url: seo.url,
       name: seo.title,
       image: {
-        '@type': 'ImageObject',
+        "@type": "ImageObject",
         url: seo.image,
       },
       mainEntityOfPage: seo.url,
     }
-
   }
 
   return (
@@ -124,10 +125,22 @@ const SEO = ({ pageTitle, pageDescription, pageBanner, pathname, page, template 
         <html lang={""} />
         <meta name="description" content={seo.description} />
         <meta name="image" content={seo.image} />
-        <meta name="gatsby-starter" content="Gatsby Starter Prismic i18n" />
+        <meta name="gatsby-starter" content="Gatsby" />
+        <meta
+          name="google-site-verification"
+          content="9nvPC-DkRzcba72WbP8MSRFmEnk1DL5Y-G_49J0GM18"
+        />
         {/* Insert schema.org data conditionally (webpage/article) + everytime (breadcrumbs) */}
-        {!page && <script type="application/ld+json">{JSON.stringify(schemaOrgWebPage)}</script>}
-        {page && <script type="application/ld+json">{JSON.stringify(schemaArticle)}</script>}
+        {!page && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaOrgWebPage)}
+          </script>
+        )}
+        {page && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaArticle)}
+          </script>
+        )}
         {/* <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script> */}
         <body className={template} />
       </Helmet>
@@ -135,16 +148,17 @@ const SEO = ({ pageTitle, pageDescription, pageBanner, pathname, page, template 
         desc={seo.description}
         image={seo.image}
         title={seo.title}
-        type={page ? 'article' : 'website'}
+        type={page ? "article" : "website"}
         url={seo.url}
         //locale={ogLanguage}
         name={facebook}
       />
-      <Twitter 
-        title={seo.title} 
-        image={seo.image} 
-        desc={seo.description} 
-        username={twitter} />
+      <Twitter
+        title={seo.title}
+        image={seo.image}
+        desc={seo.description}
+        username={twitter}
+      />
     </>
   )
 }
@@ -168,7 +182,7 @@ SEO.defaultProps = {
   pathname: null,
   page: false,
   //node: null,
-  locale: 'fr-fr',
+  locale: "fr-fr",
 }
 
 const query = graphql`
@@ -186,6 +200,5 @@ const query = graphql`
         facebook
       }
     }
-    
   }
 `
