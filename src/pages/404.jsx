1bee82c755dfb0b5df3f10584e257a534853d14d
ahmedghazi <atmet.ghazi@gmail.com>
2022-03-24 14:50:42 +0100

    build

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from "react"
 
 const Page404 = () => {
   return (
-    <div className='page-404'>
-      <div className='inner p-md'>
-        <h1>Page Not Found</h1>
-        <p>The page requested couldn't be found</p>
-        <a href='/'>Back home</a>
+    <div className="page-404 pt-header-height">
+      <div className="inner p-md">
+        <h1>Page non trouvée</h1>
+        <p>La page demandée est introuvable :(</p>
+        <a href="/">Retour à l'accueil</a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page404;
+export default Page404
