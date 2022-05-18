63b4c964671288b9185db29b2bb75a4baf09caa5
ahmedghazi <atmet.ghazi@gmail.com>
2022-05-18 16:32:15 +0200

    sources fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,4 +2,3 @@ import {_wrapRootElement, _wrapPageElement} from './wrapPageElement'
 
 export const wrapRootElement = _wrapRootElement
 export const wrapPageElement = _wrapPageElement
-// export const onClientEntry = _onClientEntry
